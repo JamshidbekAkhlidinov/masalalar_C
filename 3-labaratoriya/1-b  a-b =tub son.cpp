a0834f0f7fc4fc17d90c582f3d452ff1619aae4b
AxlidnovJ <jamshidbekaxlidinovtatu@gmail.com>
2021-12-04 14:17:45 +0500

    updates

--- 3-labaratoriya/1-b  a-b =tub son.cpp
+++ 3-labaratoriya/1-b  a-b =tub son.cpp
@@ -5,18 +5,25 @@ int main(){
 	int a,b,c,i,k;
 	cout<<"a="; cin>>a;
 	cout<<"b="; cin>>b;
-	int tub[b-a+1]
+	int tub[b-a+1];
 	for(int i=a; i<=b; i++){
 		for(int y=1; y<=i; y++){
 			if(i%y==0){
 				k++;
 			}
 		}
-		if(k>2) {
+		if(k<=2) {
 			c++;
-			tub[k]= i;
+			tub[c]= i;
 		}
 		k = 0 ;
 	}
+	float s;
+	for(int o=1; o<=c; o++){
+		cout<<tub[o]<<" ";
+		s = s + tub[o];
+	}
+	
+	cout<<"\nS => "<<s/c;
 	
 }
