aae39dcbd49e029ad0ca7ec7c4eaf2625b2e8d1f
AxlidnovJ <jamshidbekaxlidinovtatu@gmail.com>
2021-12-04 14:03:06 +0500

    updates files

--- 3-labaratoriya/1-top 3burchak.cpp
+++ 3-labaratoriya/1-top 3burchak.cpp
@@ -2,10 +2,27 @@
 
 using namespace std;
 
+float tashqiR(float c){
+	float R;
+	R = c/2;
+	return R;
+}
+
+float ichkiR(float a,float b, float c){
+	float R;
+	R = a*b/c;
+	return R;
+}
+
+
 int main(){
 	float a,b,c;
 	cout<<"A =>";cin>>a;
 	cout<<"B =>";cin>>b;
 	cout<<"C =>";cin>>c;
+	
+	cout<<"Tashqi R="<<tashqiR(c);
+	cout<<"\nIchki R="<<ichkiR(a,b,c);
+	
 	
 }
