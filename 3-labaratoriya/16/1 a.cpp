37493fb48755d65bd97b4a90811acee534b26517
AxlidnovJ <jamshidbekaxlidinovtatu@gmail.com>
2021-12-08 18:52:29 +0500

    add files

---
+++ 3-labaratoriya/16/1 a.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	int n,k;
+	string p[7] = {"Duyshanba", "Seyshanba","Chorshanba","Payshanba","Juma","SHnaba","Yakshanba"};
+	cout<<"n="; cin>>n;
+	cout<<"Bugun "<<p[n-1];
+	cout<<"\nKecha "<<p[n-2];
+}
