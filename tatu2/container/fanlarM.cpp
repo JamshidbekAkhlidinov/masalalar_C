871784da6cd83bba93e9675c60f644b432a4f0f1
Jamshidbek Axlidinov <jamshidbekaxlidinovtatu@gmail.com>
2022-05-05 18:02:00 +0500

    Update fanlarM.cpp

--- tatu2/container/fanlarM.cpp
+++ tatu2/container/fanlarM.cpp
@@ -2,114 +2,115 @@
 #include<string.h>
 using namespace std;
 
-	class universitet{
+	class fanlar{
 		public:
-			string nomi,rektorFIO;
-			int x_soni,f_soni,t_soni,p_soni;
+			string nomi,oqituvchiFIO,fan_nomi;
+			int tort_soni,uch_soni,besh_soni,o_soni;
 			
-			void kiritish(universitet a[], int n){
+			void kiritish(fanlar a[], int n){
 				for ( int i=1; i<=n; i++){
 					cout<<i<<"- ma'lumot\n";
 					cout<<"Nomi "; cin>>a[i].nomi;
-					cout<<"rektorFIO "; cin>>a[i].rektorFIO;
-					cout<<"f_soni "; cin>>a[i].f_soni;
-					cout<<"x_soni "; cin>>a[i].x_soni;
-					cout<<"t_soni "; cin>>a[i].t_soni;
-					cout<<"p_soni "; cin>>a[i].p_soni;
+					cout<<"oqituvchiFIO "; cin>>a[i].oqituvchiFIO;
+					cout<<"uch_soni "; cin>>a[i].uch_soni;
+					cout<<"tort_soni "; cin>>a[i].tort_soni;
+					cout<<"besh_soni "; cin>>a[i].besh_soni;
+					cout<<"o_soni "; cin>>a[i].o_soni;
 					
 				}
 			}
 			
-		void chiqarish(universitet a[], int n){
+		void chiqarish(fanlar a[], int n){
 			string qidir;
 			int qidir2;
-			cout<<"Nimsi qidirmoqchisiz?\n1- Nomi\n2- rektorFIO, \n3 - f_soni, \n4- x_soni, \n5- t_soni, \n6-p_soni\n\nn=";
+			cout<<"Nimsi qidirmoqchisiz?\n1- Nomi\n2- oqituvchiFIO, \n3 - uch_soni, \n4- tort_soni, \n5- besh_soni, \n6-o_soni\n\nn=";
 			int q;
 			cin>>q;
 			switch (q)
 			{
+    
 			case 1:
 				cout<<"Qidirish uchun nomni kiriting! "; cin>>qidir;
 				for(int i=1; i<=n; i++){
 					if(qidir==a[i].nomi){
 						cout<<i<<"- ma'lumot\n";
 						cout<<"Nomi "<<a[i].nomi<<endl;
-						cout<<"rektorFIO "<<a[i].rektorFIO<<endl;
-						cout<<"f_soni "<<a[i].f_soni<<endl;
-						cout<<"x_soni "<<a[i].x_soni<<endl;
-						cout<<"t_soni "<<a[i].t_soni<<endl;
-						cout<<"p_soni "<<a[i].p_soni<<endl<<endl;
+					    cout<<"oqituvchiFIO "<<a[i].oqituvchiFIO<<endl;
+						cout<<"uch_soni "<<a[i].uch_soni<<endl;
+						cout<<"tort_soni "<<a[i].tort_soni<<endl;
+						cout<<"besh_soni "<<a[i].besh_soni<<endl;
+						cout<<"o_soni "<<a[i].o_soni<<endl<<endl;
 					}
 				}
 				break;
 
 			case 2:
-				cout<<"Qidirish uchun rektorFIO kiriting! "; cin>>qidir;
+				cout<<"Qidirish uchun oqituvchiFIO kiriting! "; cin>>qidir;
 				for(int i=1; i<=n; i++){
-					if(qidir==a[i].rektorFIO){
+					if(qidir==a[i].oqituvchiFIO){
 						cout<<i<<"- ma'lumot\n";
 						cout<<"Nomi "<<a[i].nomi<<endl;
-						cout<<"rektorFIO "<<a[i].rektorFIO<<endl;
-						cout<<"f_soni "<<a[i].f_soni<<endl;
-						cout<<"x_soni "<<a[i].x_soni<<endl;
-						cout<<"t_soni "<<a[i].t_soni<<endl;
-						cout<<"p_soni "<<a[i].p_soni<<endl<<endl;
+					    cout<<"oqituvchiFIO "<<a[i].oqituvchiFIO<<endl;
+						cout<<"uch_soni "<<a[i].uch_soni<<endl;
+						cout<<"tort_soni "<<a[i].tort_soni<<endl;
+						cout<<"besh_soni "<<a[i].besh_soni<<endl;
+						cout<<"o_soni "<<a[i].o_soni<<endl<<endl;
 					}
 				}
 				break;
 			case 3:
-				cout<<"Qidirish uchun f_soni kiriting! "; cin>>qidir2;
+				cout<<"Qidirish uchun uch_soni kiriting! "; cin>>qidir2;
 				for(int i=1; i<=n; i++){
-					if(qidir2==a[i].f_soni){
+					if(qidir2==a[i].uch_soni){
 						cout<<i<<"- ma'lumot\n";
 						cout<<"Nomi "<<a[i].nomi<<endl;
-						cout<<"rektorFIO "<<a[i].rektorFIO<<endl;
-						cout<<"f_soni "<<a[i].f_soni<<endl;
-						cout<<"x_soni "<<a[i].x_soni<<endl;
-						cout<<"t_soni "<<a[i].t_soni<<endl;
-						cout<<"p_soni "<<a[i].p_soni<<endl<<endl;
+					    cout<<"oqituvchiFIO "<<a[i].oqituvchiFIO<<endl;
+						cout<<"uch_soni "<<a[i].uch_soni<<endl;
+						cout<<"tort_soni "<<a[i].tort_soni<<endl;
+						cout<<"besh_soni "<<a[i].besh_soni<<endl;
+						cout<<"o_soni "<<a[i].o_soni<<endl<<endl;
 					}
 				}
 				break;
 			case 4:
-				cout<<"Qidirish uchun x_soni kiriting! "; cin>>qidir2;
+				cout<<"Qidirish uchun tort_soni kiriting! "; cin>>qidir2;
 				for(int i=1; i<=n; i++){
-					if(qidir2==a[i].x_soni){
+					if(qidir2==a[i].tort_soni){
 						cout<<i<<"- ma'lumot\n";
 						cout<<"Nomi "<<a[i].nomi<<endl;
-						cout<<"rektorFIO "<<a[i].rektorFIO<<endl;
-						cout<<"f_soni "<<a[i].f_soni<<endl;
-						cout<<"x_soni "<<a[i].x_soni<<endl;
-						cout<<"t_soni "<<a[i].t_soni<<endl;
-						cout<<"p_soni "<<a[i].p_soni<<endl<<endl;
+					    cout<<"oqituvchiFIO "<<a[i].oqituvchiFIO<<endl;
+						cout<<"uch_soni "<<a[i].uch_soni<<endl;
+						cout<<"tort_soni "<<a[i].tort_soni<<endl;
+						cout<<"besh_soni "<<a[i].besh_soni<<endl;
+						cout<<"o_soni "<<a[i].o_soni<<endl<<endl;
 					}
 				}
 				break;
 			case 5:
-				cout<<"Qidirish uchun t_soni kiriting! "; cin>>qidir2;
+				cout<<"Qidirish uchun besh_soni kiriting! "; cin>>qidir2;
 				for(int i=1; i<=n; i++){
-					if(qidir2==a[i].t_soni){
+					if(qidir2==a[i].besh_soni){
 						cout<<i<<"- ma'lumot\n";
 						cout<<"Nomi "<<a[i].nomi<<endl;
-						cout<<"rektorFIO "<<a[i].rektorFIO<<endl;
-						cout<<"f_soni "<<a[i].f_soni<<endl;
-						cout<<"x_soni "<<a[i].x_soni<<endl;
-						cout<<"t_soni "<<a[i].t_soni<<endl;
-						cout<<"p_soni "<<a[i].p_soni<<endl<<endl;
+					    cout<<"oqituvchiFIO "<<a[i].oqituvchiFIO<<endl;
+						cout<<"uch_soni "<<a[i].uch_soni<<endl;
+						cout<<"tort_soni "<<a[i].tort_soni<<endl;
+						cout<<"besh_soni "<<a[i].besh_soni<<endl;
+						cout<<"o_soni "<<a[i].o_soni<<endl<<endl;
 					}
 				}
 				break;
 			case 6:
-				cout<<"Qidirish uchun p_soni kiriting! "; cin>>qidir2;
+				cout<<"Qidirish uchun o_soni kiriting! "; cin>>qidir2;
 				for(int i=1; i<=n; i++){
-					if(qidir2==a[i].p_soni){
+					if(qidir2==a[i].o_soni){
 						cout<<i<<"- ma'lumot\n";
 						cout<<"Nomi "<<a[i].nomi<<endl;
-						cout<<"rektorFIO "<<a[i].rektorFIO<<endl;
-						cout<<"f_soni "<<a[i].f_soni<<endl;
-						cout<<"x_soni "<<a[i].x_soni<<endl;
-						cout<<"t_soni "<<a[i].t_soni<<endl;
-						cout<<"p_soni "<<a[i].p_soni<<endl<<endl;
+					    cout<<"oqituvchiFIO "<<a[i].oqituvchiFIO<<endl;
+						cout<<"uch_soni "<<a[i].uch_soni<<endl;
+						cout<<"tort_soni "<<a[i].tort_soni<<endl;
+						cout<<"besh_soni "<<a[i].besh_soni<<endl;
+						cout<<"o_soni "<<a[i].o_soni<<endl<<endl;
 					}
 				}
 				break;
@@ -130,7 +131,7 @@ int main(){
 	string qidir;
 	cout<<"Nechta ma'lumot kiritmoqchisiz? "<<endl;
 	cin>>n;
-	universitet cup[20];
+	fanlar cup[20];
 	cup[20].kiritish(cup,n);
 	cup[20].chiqarish(cup,n);
 	return 0;	
