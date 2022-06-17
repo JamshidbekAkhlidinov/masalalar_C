b3b7aa3a61e195e56cc6b2ef18247873cea209bc
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2022-06-17 12:25:22 +0500

    Update qweqwe.cpp

--- qweqwe.cpp
+++ qweqwe.cpp
@@ -1,8 +1,18 @@
-#include <forward_list>
-#include <iostream>
+#include<conio.h>
+#include<list>
+#include<unordered_map>
+#include <typeinfo>
+#include <cstddef> // std::size_t
+#include <valarray> // std::valarray, std::slice
 using namespace std;
-int main(){
-	forward_list<int> k;
-	k.push_front(2);
-	if(k.empty())
-}
+using namespace System;
+int main(array<System::String> args)
+{std::valarray<int> sample(12);
+for (int i = 0; i < 13; ++i)
+sample[i] = i;
+std::valarray<int> bar = sample[std::slice(2, 3, 4)];
+std::cout << "slice(2, 3, 4):";
+for (std::size_t n = 0; n < bar.size(); n++)
+std::cout <<" "<< bar[n];
+std::cout << "n=";
+getch(); }
