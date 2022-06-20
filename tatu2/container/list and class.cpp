7bfd1ad0b2b035b043cbcc8eb3ddc4ce8e06ad18
Jamshidbek Axlidinov <94201013+JamshidbekAkhlidinov@users.noreply.github.com>
2022-06-20 15:20:35 +0500

    Update list and class.cpp

--- tatu2/container/list and class.cpp
+++ tatu2/container/list and class.cpp
@@ -2,53 +2,46 @@
 #include <list>
 using namespace std;
 
-class mashinalar{
+class poyezd{
 	public:
 		string nomi;
-		float tezligi;
+		float tezligi,uzunligi;
 };
 
-class avtobuslar: public mashinalar{
-	public:
-		int ot_kuchi;
-		float yoqilgi_sigimi;
-};
 
-void kirit(avtobuslar a[], int n){
+
+void kirit(poyezd a[], int n){
 	for(int i=0; i<n; i++){
 		cout<<"Nomi =>"; cin>>a[i].nomi;
 		cout<<"Tezligi =>";cin>>a[i].tezligi;
-		cout<<"ot_kuchi =>"; cin>>a[i].ot_kuchi;
-		cout<<"yoqilgi_sigimi =>";cin>>a[i].yoqilgi_sigimi;
+		cout<<"uzunligi =>"; cin>>a[i].uzunligi;
 		
 		cout<<endl;
 	}
 }
 
-void sarala(avtobuslar a[], int n){
+void sarala(poyezd a[], int n){
 	for(int i=0; i<n; i++){
 		for(int j=0; j<i; j++){
-			if(a[i].ot_kuchi>a[j].ot_kuchi){
+			if(a[i].uzunligi>a[j].uzunligi){
 				swap(a[i],a[j]);
 			}
 		}
 	}
 }
 
-void chiqar(avtobuslar a[], int n){
+void chiqar(poyezd a[], int n){
 	for(int i=0; i<n; i++){
 		cout<<"Nomi =>"<<a[i].nomi<<endl;
 		cout<<"Tezligi =>"<<a[i].tezligi<<endl;
-		cout<<"ot_kuchi =>"<<a[i].ot_kuchi<<endl;
-		cout<<"yoqilgi_sigimi =>"<<a[i].yoqilgi_sigimi<<endl<<endl
-		;
+		cout<<"uzunligi =>"<<a[i].uzunligi<<endl<<endl;
 	}
 }
 
 int main(){
 	int n,i;
 	cin>>n;
-	avtobuslar a[n];
+	poyezd a[n];
 	kirit(a,n);
 	cout<<"\n\nSaralangan chiqarish \n";
 	sarala(a,n);
